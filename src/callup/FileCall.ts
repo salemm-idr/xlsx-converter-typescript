7be378d4620e50647853002d84ee658705f1bd1c
salemm-idr <ejuarez@idr-tech.com>
2020-03-05 13:00:22 -0600

    :bug: async operations in :construction: =====50%

--- src/callup/FileCall.ts
+++ src/callup/FileCall.ts
@@ -54,21 +54,22 @@ export class FileCall {
   }
 
   public async constructWorkSheet(workbook: WorkBook) {
-    return new Promise<object>((resolve, reject) => {
+    return new Promise<any>((resolve, reject) => {
       setTimeout(() => console.log("construyendo sheet 🕵"), 200);
 
       let tabs: string[] = workbook.SheetNames;
       let worksheet: WorkSheet;
       console.log(tabs, "in filecall 👌");
 
-      let daFile = tabs.map((tab, index) => {
+      let daFile: any = tabs.map((tab, index) => {
         let toSave = {} as toWrite;
         worksheet = workbook.Sheets[tab];
         console.log(tab, "nombre de la tabla individual 🚀");
-        let data: (string | number)[] = xlsx.utils.sheet_to_json(worksheet, {
+        let data = xlsx.utils.sheet_to_json(worksheet, {
           header: 1
         });
         //this.constructNewJson(data);
+        //*diferencia
         // let stream = xlsx.stream.to_json(worksheet, { raw: true });
         // var conv = new Transform({ writableObjectMode: true });
         // conv._transform = function(obj, e, cb) {
@@ -79,19 +80,21 @@ export class FileCall {
         // );
         // stream.pipe(conv);
         // conv.pipe(myWriteStream);
-        toSave.name = tab;
-        toSave.hojaAoA = data;
+        // toSave.name = tab;
+        // toSave.hojaAoA = data;
         //this.writeJsonToFolder(toSave);
 
-        return toSave;
-        //return data;
+        //return toSave;
+        return data;
       });
-
-      resolve(daFile);
-    });
+      setTimeout(() => {
+        console.log("resolviendo json");
+        resolve(daFile[0]); //!por que la respuesta venia con un extra []
+      }, 3000);
+    }).then(value => this.constructNewJson(value));
   }
 
-  public async writeJsonToFolder(wrote: any) {
+  /*  public async writeJsonToFolder(wrote: any) {
     setTimeout(() => console.log("Escribiendo nuevo AoA 🖨"), 200);
     return new Promise<string>((resolve, reject) => {
       // let writeStreamer = filesystem.createWriteStream(
@@ -103,29 +106,23 @@ export class FileCall {
       );
       resolve(wrote[0].name);
     }).then(name => this.constructNewJson(name));
-  }
+  } */
 
-  public async constructNewJson(name: string) {
+  public async constructNewJson(grabado: any) {
     setTimeout(() => console.log("constuyendo nuevo json"), 200);
     return new Promise((resolve, reject) => {
       //*version streamer
-      let myReadStream = filesystem.createReadStream(
-        `src\\outputs\\${name}.json`
-      );
+      // let myReadStream = filesystem.createReadStream(
+      //   `src\\outputs\\${name}.json`
+      // );
       // let myWriteStream = filesystem.createWriteStream(
       //   `src\\tiras\\${name}.json`
       // );
-      myReadStream.on("data", chunk => {
-        let buf = Buffer.from(chunk, "utf-8");
-        let grabado = JSON.stringify(buf);
-        console.table(grabado.slice(0, 20));
-      });
 
       //*version readfile sync
       /* let data = filesystem.readFileSync(`src\\outputs\\${name}.json`, "utf8");
-      let grabado = JSON.parse(data);
-      //console.log(grabado.slice(0, 20));
-
+      let grabado = JSON.parse(data); */
+      console.log(grabado, "segmntado");
       let dataWorked: any = [];
       grabado.forEach((element: any, index: number) => {
         const texted: any = element.map((innerText: string) => {
@@ -141,11 +138,15 @@ export class FileCall {
           this.constructedSearch = texted;
           dataWorked = grabado.slice(index + 1);
           //return setTimeout(() => resolve(dataWorked), 600);
+
           return dataWorked;
         }
-      });*/
-      //resolve(dataWorked);
-    }); //.then(dataWorked => this.composeNewObject(dataWorked));
+      });
+      setTimeout(() => {
+        console.log(dataWorked, "Procesando archivo...");
+        resolve(dataWorked);
+      }, 3000);
+    }).then(dataWorked => this.composeNewObject(dataWorked));
   }
 
   public async composeNewObject(dataWorked: any) {
@@ -159,15 +160,19 @@ export class FileCall {
         });
         return xFile;
       });
+      let myWriteStream = filesystem.createWriteStream(
+        `src\\tiras\\streamThenVersion.json`
+      );
+      myWriteStream.write(JSON.stringify(nodos, null, 2), error => {
+        if (error) {
+          console.log("hay un errr al grabar con stream", error);
+        }
+      });
       //*stream version
       // let myReadStream = filesystem.createReadStream(
       //   `src\\outputs\\${name}.json`
       // );
-      let myWriteStream = filesystem.createWriteStream(
-        `src\\tiras\\streamXXX.json`
-      );
 
-      myWriteStream.write(JSON.stringify(nodos, null, 2));
       //*writeSync version
       // filesystem.writeFileSync(
       //   `src\\tiras\\EXITO2callBack.json`,
@@ -176,20 +181,20 @@ export class FileCall {
 
       console.log("grabando nuevo JSON ✍️");
       resolve();
-    }).then(() => this.writeNewExcel());
+    }); //.then(() => this.writeNewExcel());
   }
   public async writeNewExcel() {
     return new Promise((resolve, reject) => {
       setTimeout(() => console.log("Escribe nuevo excel 👷"), 200);
-      let myReadStream = filesystem.createReadStream(
-        `src\\tiras\\streamXXX.json`
-      );
-      let myWriteStream = filesystem.createWriteStream(
-        "src\\constructedFile\\streamExcel.xlsx"
-      );
-      myReadStream.on("data", chunk => {
-        console.log(chunk);
-      });
+      // let myReadStream = filesystem.createReadStream(
+      //   `src\\tiras\\streamXXX.json`
+      // );
+      // let myWriteStream = filesystem.createWriteStream(
+      //   "src\\constructedFile\\streamExcel.xlsx"
+      // );
+      // myReadStream.on("data", chunk => {
+      //   console.log(chunk);
+      // });
       /**crea el libro de trabajo */
       const wb: WorkBook = xlsx.utils.book_new();
       /**nombre de la hoja string */
@@ -214,11 +219,12 @@ export class FileCall {
   //todo comentar las funciones y sintantic
   public async doitAll(name: string) {
     const filex: WorkBook = <WorkBook>await this.readFilex(name);
-    const constructedWorkSheet: object = await this.constructWorkSheet(filex);
-    const writeJson = await this.writeJsonToFolder(constructedWorkSheet);
+    const constructedWorkSheet: any = await this.constructWorkSheet(filex);
+    // const writeJson = await this.writeJsonToFolder(constructedWorkSheet);
     //const readJson = await this.readJsonFromFolder(writeJson);
-    //const newTable = await this.constructNewJson(writeJson);
-    //const newObject = await this.composeNewObject(newTable);
-    //const newExcel = await this.writeNewExcel();
+    // const newTable = await this.constructNewJson(constructedWorkSheet);
+    // const newObject = await this.composeNewObject(newTable);
+    // const newExcel = await this.writeNewExcel();
+    // return [filex, constructedWorkSheet, newTable, newObject, newExcel];
   }
 }
